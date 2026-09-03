01f9c72f12fa60ae18924fcd924fa5495a27a508
EternityShecn <3258551611@qq.com>
2026-09-03 17:17:00 +0800

    add: 增加世界水瓦片的位置集合

--- src/plugins/world.rs
+++ src/plugins/world.rs
@@ -1,9 +1,30 @@
+use std::collections::HashSet;
+
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 use bevy_rapier2d::{
     dynamics::RigidBody, geometry::Collider, picking_backend::RapierPickingPlugin,
     plugin::RapierPhysicsPlugin, render::RapierDebugRenderPlugin,
 };
+
+#[derive(Resource)]
+pub struct WaterSet {
+    set: HashSet<IVec2>,
+}
+
+impl WaterSet {
+    pub fn insert(&mut self, pos: IVec2) {
+        self.set.insert(pos);
+    }
+
+    pub fn is_water(&self, pos: IVec2) -> bool {
+        self.set.contains(&pos)
+    }
+
+    pub fn remove(&mut self, pos: IVec2) {
+        self.set.remove(&pos);
+    }
+}
 
 pub struct WorldPlugin;
 
@@ -30,12 +51,21 @@ fn setup(mut commands: Commands, asset_servet: Res<AssetServer>) {
     info!("创建世界地图");
 }
 
-fn build_col_map(mut commands: Commands, query: Query<(Entity, &IntGridCell), Added<IntGridCell>>) {
-    for (entity, cell) in query {
+fn build_col_map(
+    mut commands: Commands,
+    query: Query<(Entity, &IntGridCell, &GridCoords), Added<IntGridCell>>,
+    mut water_set: ResMut<WaterSet>,
+) {
+    for (entity, cell, coord) in query {
         if cell.value == 1 {
             commands
                 .entity(entity)
                 .insert((Collider::cuboid(8.0, 8.0), RigidBody::Fixed));
+
+            water_set.insert(IVec2 {
+                x: coord.x,
+                y: coord.y,
+            });
         }
     }
 }
